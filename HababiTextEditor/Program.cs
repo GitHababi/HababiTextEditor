f61af45e56c7826f8a8fc969496f91ae1d678f80
Hababi <76075036+GitHababi@users.noreply.github.com>
2022-05-25 17:55:55 -0700

    Added Project References

--- HababiTextEditor/Program.cs
+++ HababiTextEditor/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System.Threading;
 using System.IO;
+using Terminal.Gui;
+using NStack;
 namespace HTE
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
-            
         }
     }
 }
