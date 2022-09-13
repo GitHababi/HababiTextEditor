4d55d09c927eae5d0f0b1ad2470eb6a0fb460656
Hababi <76075036+GitHababi@users.noreply.github.com>
2022-09-12 20:33:16 -0700

    Revert "Added Project References"
    This reverts commit f61af45e56c7826f8a8fc969496f91ae1d678f80.

--- HababiTextEditor/Program.cs
+++ HababiTextEditor/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System.Threading;
 using System.IO;
-using Terminal.Gui;
-using NStack;
 namespace HTE
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
+            
         }
     }
 }
